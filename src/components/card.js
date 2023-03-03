633105f680f829cae6a9953418f56be2643faace
sheriff oladimeji <dimejiademola5@gmail.com>
2023-03-03 16:32:12 +0100

    modify card.js

--- src/components/card.js
+++ src/components/card.js
@@ -11,7 +11,7 @@ return (
 <a href={props.card.googleMapsUrl}  target="_blank" rel="noreferrer" className="card-link">View on Google Maps</a>
   </div>
   <h2  className="card-title">{props.card.title}</h2>
-  <h4 className="card-date">{props.card.startDate}-{props.card.endDate}</h4>
+  <h3 className="card-date">{props.card.startDate}-{props.card.endDate}</h3>
   <p className="card-desc">
     {props.card.description}
   </p>
