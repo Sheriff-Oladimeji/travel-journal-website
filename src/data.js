3fedfd7560e1eaa992bf67ba9a32296500467121
sheriff oladimeji <dimejiademola5@gmail.com>
2023-03-03 09:20:04 +0100

    modify files

---
+++ src/data.js
@@ -0,0 +1,42 @@
+import img1 from "./images/img1.png"
+import img2 from "./images/img2.png"
+import img3 from "./images/img3.png"
+  const data = [
+  {
+    id: 1,
+    title: "Mount Fuji",
+    location: "JAPAN",
+    googleMapsUrl:
+      "https://www.google.com.ng/maps/place/Mount+Fuji/@35.3624971,138.7256467,15z/data=!4m5!3m4!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634?hl=en",
+    startDate: "12 Jan, 2023",
+    endDate: "24 Jan, 2023",
+    description:
+      "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+    image:img1,
+  },
+  {
+    id: 2,
+    title: "Sydney Opera House ",
+    location: "AUSTRALIA",
+    googleMapsUrl:
+      "https://www.google.com.ng/maps/place/Australia/@-12.3041061,74.5229837,3z/data=!4m5!3m4!1s0x2b2bfd076787c5df:0x538267a1955b1352!8m2!3d-25.274398!4d133.775136?hl=en",
+    startDate: "27 May, 2021",
+    endDate: "8 Jun, 2021",
+    description:
+      "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings",
+    image:img2,
+  },
+  {
+    id: 3,
+    title: "Geirangerfjord",
+    location: "NORWAY",
+    googleMapsUrl:
+      "https://www.google.com.ng/maps/place/Geirangerfjord/@62.1049113,7.0051735,12z/data=!3m1!4b1!4m5!3m4!1s0x46169d427b268c51:0xb8c99540dcc397fe!8m2!3d62.101506!4d7.0940817?hl=en",
+    startDate: "01 Oct, 2021",
+    endDate: "18 Nov, 2021",
+    description:
+      "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality..",
+    image:img3,
+  }
+];
+export default data
