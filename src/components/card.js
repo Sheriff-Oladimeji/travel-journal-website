25dc40f5b02f5e143e59d4d343c15dfd0631ac53
sheriff oladimeji <dimejiademola5@gmail.com>
2023-03-03 15:55:11 +0100

    modify files

--- src/components/card.js
+++ src/components/card.js
@@ -8,6 +8,7 @@ return (
 <div>
   <img src={icon} alt=""/><p>{props.card.location}</p>
 </div>
+<a href={props.card.googleMapsUrl}  target="_blank">View on Google Maps</a>
   </div>
   <h2>{props.card.title}</h2>
   <h4 className="small">{props.card.startDate}-{props.card.endDate}</h4>
